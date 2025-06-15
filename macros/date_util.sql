14e6fe015f36e995aeb840d205b4ba8702b90014
Pappala Bharadwaj Naidu <180030034cse@gmail.com>
2025-06-15 16:19:32 +0000

    made some changes

--- macros/date_util.sql
+++ macros/date_util.sql
@@ -1,26 +1,26 @@
-{% macro function1(x) %}
+{% macro function1(start_at) %}
 
-CASE WHEN TO_TIMESTAMP({{x}}) < CURRENT_DATE THEN 'PAST'
+CASE WHEN TO_TIMESTAMP({{start_at}}) < CURRENT_DATE THEN 'PAST'
 ELSE 'FUTURE' END
 
 {% endmacro %},
 
-{% macro get_season(x)%}
+{% macro get_season(start_at)%}
 
-CASE WHEN MONTH(TO_TIMESTAMP({{x}})) in (12,1,2)
+CASE WHEN MONTH(TO_TIMESTAMP({{start_at}})) in (12,1,2)
     THEN 'WINTER'
-    WHEN MONTH(TO_TIMESTAMP({{x}})) in (3,4,5)
+    WHEN MONTH(TO_TIMESTAMP({{start_at}})) in (3,4,5)
     THEN 'SPRING'
-    WHEN MONTH(TO_TIMESTAMP({{x}})) in (6,7,8)
+    WHEN MONTH(TO_TIMESTAMP({{start_at}})) in (6,7,8)
     THEN 'SUMMER'
     ELSE 'AUTUMN'
     END
 {% endmacro  %}
 
-{%macro DAY_TYPE (x) %}
+{%macro DAY_TYPE (start_at) %}
 
 CASE
-WHEN DAYNAME(TO_TIMESTAMP({{x}})) in ('Sat','Sun')
+WHEN DAYNAME(TO_TIMESTAMP({{start_at}})) in ('Sat','Sun')
 THEN 'WEEKEND'
 ELSE 'BUSINESSDAY'
 END 
