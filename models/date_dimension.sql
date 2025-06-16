93c805199d1581114c501c6eced7f8840a2dbde5
Pappala Bharadwaj Naidu <180030034cse@gmail.com>
2025-06-16 07:04:34 +0000

    uncommented the snippet

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -5,10 +5,13 @@ DATE(TO_TIMESTAMP(START_AT)) AS DATE_STARTED_AT,
 HOUR(TO_TIMESTAMP(START_AT)) AS HOUR_STARTED_AT,
     {{DAY_TYPE('START_AT')}} AS DAY_TYPE,
     {{get_season('START_AT')}}
-    -- {{function1('START_AT')}}
+    {{function1('START_AT')}}
 from
 {{ source('my_source', 'bike') }}
-where START_AT != 'started_at' and START_AT != 'started_at'
+-- where START_AT != 'start_at' and START_AT != 'start_at'
+WHERE START_AT IS NOT NULL
+  AND START_AT != ''
+
 )
 select
 *
