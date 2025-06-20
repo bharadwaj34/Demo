7b6a77bf8c669f31219b6ca868795628dbdcbc64
Pappala Bharadwaj Naidu <180030034cse@gmail.com>
2025-06-20 13:22:55 +0000

    Incremental materialized

---
+++ models/fct/fct_ratings.sql
@@ -0,0 +1,28 @@
+{{
+    config(
+        materialized = 'incremental',
+        on_schema_change ='fail'
+    )
+}}
+with raw_ratings as (
+    select * from {{ ref('raw_rating') }}
+
+)
+select 
+    user_id,
+    movie_id,
+    rating,
+    rating_timestamp
+from raw_ratings
+where rating is not null 
+
+{% if is_incremental() %}
+  and rating_timestamp > (select max(rating_timestamp)) from {{this}}
+{% endif%}
+
+
+-- raw_rating (base table) = 5pm -> 6pm 
+-- fct_ratings(fct table) = 5pm 
+
+-- this = fct_ratings 
+--6>5
